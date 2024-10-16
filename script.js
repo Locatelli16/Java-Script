b88a776624dea39579ff348a1f3bddd90e804197
Locatelli16 <164167466+Locatelli16@users.noreply.github.com>
2024-10-16 08:09:27 -0300

    ajuste 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mudarCor() {
-    const titulo = document.getElementByld('Título');
+    const titulo = document.getElementByld('titulo');
     titulo.style.color = titulo.style.color === 'red' ? 'blue' : 'red';
 }
